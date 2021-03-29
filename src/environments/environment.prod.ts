ad8cdceb1474d900aa55b79cb53654f82be4a720
uriart <loiro_8@hotmail.com>
2021-03-29 20:56:51 +0200

    Commit prueba pro

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  auth: {
+    clientID: '1oVAezqLX77ftKY3aKHX3Cr86ag2guga',
+    domain: 'dev-r071w8pk.eu.auth0.com', // e.g., you.auth0.com
+    audience: 'https://dev-r071w8pk.eu.auth0.com/api/v2/',
+    auth0RedirectUri: 'https://uriart.github.io/programa-powerlifting/home', // URL to return to after auth0 login
+    auth0ReturnTo: 'https://uriart.github.io/programa-powerlifting/home', // URL to return to after auth0 logout
+    scope: 'openid profile'
+  }
 };
