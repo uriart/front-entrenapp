c636140a48dae1a6529fb09d7c5a608ee4849c1e
DESKTOP-IM9483I\Uri <loiro_8@hotmail.com>
2022-05-09 18:28:39 +0200

    prueba deploy

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = expreess();
+
+app.user(express.static('./dist/candito-program'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/candito-program/'})
+);
+
+app.listen(process.env.PORT || 8080);
