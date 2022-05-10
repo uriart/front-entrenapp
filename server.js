7069fc3728e19650ed79b275131a3119ff5521de
DESKTOP-IM9483I\Uri <loiro_8@hotmail.com>
2022-05-10 09:29:15 +0200

    Ahora sí?

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/front-program'));
+app.use(express.static('./dist/authapp'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/front-program/'}),
+    res.sendFile('index.html', {root: 'dist/authapp/'}),
 );
 
 // Start the app by listening on the default Heroku port
