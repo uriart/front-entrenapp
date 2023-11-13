16c7d42d72dfc703b174462b232b747035730528
uriart <loiro_8@hotmail.com>
2023-11-13 23:21:47 +0100

    [master] fix environment vars

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
   auth: {
-    domain: "https://dev-r071w8pk.eu.auth0.com/",
-    clientId: "aZr46as8jeMqGYrP7dvk90wqIWc2upBy",
+    domain: window['env']['AUTH_ISSUER'],
+    clientId: window['env']['CLIENT_ID'],
     redirectUri: window.location.origin,
-    audience : "https://users-api-test2.herokuapp.com/"
+    audience : window['env']['AUTH_AUDIENCE']
   },
   dev: {
     apiUrl: 'entrenapp-api:8080'
