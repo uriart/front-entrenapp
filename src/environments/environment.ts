cf83d142847302b7ed14bc605295927b774e113c
uriart <loiro_8@hotmail.com>
2023-11-18 13:31:48 +0100

    [mater] provisional vars

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   auth: {
-    domain: window['env']['AUTH_ISSUER'],
-    clientId: window['env']['CLIENT_ID'],
+    domain: 'https://dev-r071w8pk.eu.auth0.com/',
+    clientId: 'aZr46as8jeMqGYrP7dvk90wqIWc2upBy',
     redirectUri: window.location.origin,
-    audience : window['env']['AUTH_AUDIENCE']
+    audience: 'https://users-api-test2.herokuapp.com/'
   },
   dev: {
-    apiUrl: 'entrenapp-api:8080'
+    apiUrl: 'entrenapp-backend-service:8080'
   }
 };
 
