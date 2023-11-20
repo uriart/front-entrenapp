578c7e99cdd8afa894610278b887bb578899dfb7
uriart <loiro_8@hotmail.com>
2023-11-20 09:24:45 +0100

    [master] fix domain

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   auth: {
-    domain: 'https://dev-r071w8pk.eu.auth0.com/',
+    domain: 'https://dev-r071w8pk.eu.auth0.com',
     clientId: 'aZr46as8jeMqGYrP7dvk90wqIWc2upBy',
     redirectUri: window.location.origin,
     audience: 'https://users-api-test2.herokuapp.com/'
