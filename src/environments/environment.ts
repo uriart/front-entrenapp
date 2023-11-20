81ca4f0ef46cf89c33cd6e1adf2e7b6975ae8585
uriart <loiro_8@hotmail.com>
2023-11-20 09:36:42 +0100

    [master] fix redirect uri

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   auth: {
     domain: 'https://dev-r071w8pk.eu.auth0.com',
     clientId: 'aZr46as8jeMqGYrP7dvk90wqIWc2upBy',
-    redirectUri: window.location.origin,
+    redirectUri: window.location.origin + '/entrenapp/',
     audience: 'https://users-api-test2.herokuapp.com/'
   },
   dev: {
