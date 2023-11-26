e2c10931008018be13ca68bfaa684493ba9211d0
uriart <loiro_8@hotmail.com>
2023-11-26 17:39:18 +0100

    [master] Fix api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,7 @@
 export const environment = {
   production: true,
-  auth: {
-    domain: 'https://dev-r071w8pk.eu.auth0.com',
-    clientId: 'aZr46as8jeMqGYrP7dvk90wqIWc2upBy',
-    redirectUri: window.location.origin + '/entrenapp/',
-    audience: 'https://users-api-test2.herokuapp.com/'
-  },
   dev: {
-    apiUrl: 'http://localhost:8080'
+    apiUrl: 'api-entrenapp-service'
   }
 };
 
